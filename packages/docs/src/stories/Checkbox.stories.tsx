9be2cf8931600edc14a9cf6b703d40e046534c1b
Henrique Pereira <henrique802011@gmail.com>
2023-01-31 22:42:03 -0300

    h-ignite-ui - componentes do design system

--- packages/docs/src/stories/Checkbox.stories.tsx
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -11,7 +11,7 @@ export default {
         <>
           <Box
             as="label"
-            css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+            css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
           >
             {Story()}
             <Text size="sm">Accept terms of use</Text>
