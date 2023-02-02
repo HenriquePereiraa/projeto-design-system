609a742f36fd46e61ed2f10294cc509d468d72fe
Henrique Pereira <henrique802011@gmail.com>
2023-02-01 21:09:56 -0300

    hp-ignite-ui - controle nos componentes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/henriquepereiraa.png',
     alt: 'Henrique Pereira',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -7,6 +7,29 @@ export default {
   args: {
     children:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio possimus dolore nihil eum voluptate suscipit, porro illum velit, harum minima, tempore autem. Dolor doloremque consequuntur quo non repellendus reiciendis aliquid!',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
